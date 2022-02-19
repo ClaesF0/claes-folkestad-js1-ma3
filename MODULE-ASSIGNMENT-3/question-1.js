576e79eb6a24ad9e20bafd93a9b1ca2299704871
ClaesF0 <claes.folkestad@stud.noroff.no>
2022-02-19 19:06:35 +0100

    Claes-js1-ma3

---
+++ MODULE-ASSIGNMENT-3/question-1.js
@@ -0,0 +1,14 @@
+/*
+Question 1
+Convert the function below to an arrow function:
+*/
+/*
+function getRemainder(a,b) {
+    return a % b;
+    }
+*/
+console.log("Question 1")
+const getRemainder = (a, b) => a % b; 
+
+getRemainder();
+console.log("The remainder is", getRemainder(10, 3))
